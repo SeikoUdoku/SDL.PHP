a1f3eb8357c66c8c0765b7994590014d740492a5
HarutoSugimoto <54029369+HarutoSugimoto@users.noreply.github.com>
2022-03-14 20:44:55 +0900

    [update] コレクション関連のリファクタリングを行う。 (#5)

--- src/Collection/IReadonlyCollection.php
+++ src/Collection/IReadonlyCollection.php
@@ -13,10 +13,38 @@ interface IReadonlyCollection extends Countable, IteratorAggregate
     // 抽象関数
     // ================================================================
     /**
+     * 要素の取得を試みる。
+     *
+     * @param int|string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function tryGet(int|string $key, mixed $default = null) : mixed;
+
+
+    /**
      * 要素を取得する。
      *
      * @param int|string $key
      * @return mixed
      */
     public function get(int|string $key) : mixed;
+
+
+    /**
+     * 特定のキーが含まれるか判定する。
+     *
+     * @param int|string $key
+     * @return bool
+     */
+    public function containsKey(int|string $key) : bool;
+
+
+    /**
+     * 特定の値が含まれるか判定する。
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public function containsValue(mixed $value) : bool;
 }
