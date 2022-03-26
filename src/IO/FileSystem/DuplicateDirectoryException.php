06418075db34d17a312497cf011bcb854ac6e1e4
HarutoSugimoto <54029369+HarutoSugimoto@users.noreply.github.com>
2022-03-26 12:00:19 +0900

    [add]ローカルのファイルシステムにおけるディレクトリ・ファイルの操作機能を実装する。

---
+++ src/IO/FileSystem/DuplicateDirectoryException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Jp\Skud\Sdl\IO\FileSystem;
+
+use Jp\Skud\Sdl\IO\IOException;
+
+/**
+ * ディレクトリが重複した場合の例外クラス
+ */
+class DuplicateDirectoryException extends IOException
+{
+}
---
+++ src/IO/FileSystem/DuplicateItemException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Jp\Skud\Sdl\IO\FileSystem;
+
+use Jp\Skud\Sdl\IO\IOException;
+
+/**
+ * ファイルシステムにおける項目が重複した場合の例外クラス
+ */
+class DuplicateItemException extends IOException
+{
+}
